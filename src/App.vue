583c158c15cbb4c7e6deb9118a467e35648cf37d
Volkov Alexander <volkov.525@gmail.com>
2022-04-10 16:09:22 +0300

    add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template lang="pug">
-.container
-	router-view
+q-layout(view="hHh lpR fFf")
+	q-drawer(show-if-above v-model="leftDrawerOpen" side="left" overlay bordered)
+	q-page-container
+		.container
+			router-view
+	q-footer(bordered)
+		q-toolbar
+			q-btn(flat round dense icon="mdi-menu" @click="leftDrawerOpen = !leftDrawerOpen").q-mr-sm
+
 </template>
 
-<script setup="setup" lang="ts"></script>
+<script setup="setup" lang="ts">
+import { ref, reactive } from 'vue'
+const leftDrawerOpen = ref(false)
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +23,27 @@
 	text-align: center;
 	color: #2c3e50;
 }
-body {
+
+.q-page-container {
 	background: url('@/assets/login.jpg');
 	margin: 0;
 	padding: 0;
 	width: 100%;
 	height: 100vh;
 }
+
 .container {
-	height: 100vh;
+	height: 100%;
 	width: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+}
+
+.q-drawer {
+	background: $dark1;
+}
+.q-footer {
+	background: $dark;
 }
 </style>
