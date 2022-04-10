998e6057c74d22538326a94fab299b38bef7643a
Volkov Alexander <volkov.525@gmail.com>
2022-04-10 15:39:31 +0300

    Small fixes. Not so important.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -25,8 +25,8 @@ const password = ref('')
 const buttons = [
 	{ label: 'Продолжить с Azure', icon: 'azure' },
 	{ label: 'Продолжить с Google', icon: 'google' },
-	{ label: 'Продолжить с Facebook', icon: 'fb' },
-	{ label: 'Продолжить с Twitter', icon: 'twitter' },
+	// { label: 'Продолжить с Facebook', icon: 'fb' },
+	// { label: 'Продолжить с Twitter', icon: 'twitter' },
 ]
 </script>
 
@@ -34,9 +34,9 @@ const buttons = [
 .card {
 	padding: 1rem;
 	padding-top: 2rem;
-	min-height: 50%;
+	min-height: 300px;
 	min-width: 400px;
-	border: 1px solid #ffffffaa;
+	border: 1px solid rgba($color: #ffffff, $alpha: 0.3);
 	border-radius: 0.3rem;
 	background: rgba($color: #000000, $alpha: 0.7);
 	box-shadow: -8px -8px 6px rgba($color: #000000, $alpha: 0.5),
