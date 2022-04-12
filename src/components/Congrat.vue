e627a8043235c2f1f9a632c6a4a808b5537b20b2
Volkov Alexander <volkov.525@gmail.com>
2022-04-12 15:07:55 +0300

    green border

--- src/components/Congrat.vue
+++ src/components/Congrat.vue
@@ -18,7 +18,7 @@
 	padding-top: 2rem;
 	min-height: 300px;
 	min-width: 400px;
-	border: 1px solid rgba($color: #ffffff, $alpha: 0.3);
+	border: 1px solid green;
 	border-radius: 0.3rem;
 	background: rgba($color: #000000, $alpha: 0.7);
 	box-shadow: -8px -8px 6px rgba($color: #000000, $alpha: 0.5),
